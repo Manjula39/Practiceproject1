9882b6c73aa8cd7a081071981bf767545f0acf79
Manjula36 <manjulavdt@gmail.com>
2024-02-19 16:08:39 +0530

    Add project files.

---
+++ Nunitproject1/Stud.cs
@@ -0,0 +1,41 @@
+﻿using Practiceproject1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunitproject1
+{
+    [TestFixture]
+    public class Stud
+    {
+        [Test]
+        public void TestStudentName()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Name = "Manju";
+
+            // Act
+            string studentName = student.Name;
+
+            // Assert
+            Assert.AreEqual("Manju", studentName);
+        }
+
+        [Test]
+        public void TestStudentAddress()
+        {
+            // Arrange
+            Student student = new Student();
+            student.Address = "Gandhinagar 4th cross Bly";
+
+            // Act
+            string studentAddress = student.Address;
+
+            // Assert
+            Assert.AreEqual("Gandhinagar 4th cross Bly", studentAddress);
+        }
+    }
+}
---
+++ Nunitproject1/Teach.cs
@@ -0,0 +1,26 @@
+﻿using Practiceproject1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nunitproject1
+{
+    [TestFixture]
+    public class Teach
+    {
+        [Test]
+        public void Test_CreateTeacher()
+        {
+            // Arrange
+            var teacher = new Teacher();
+            teacher.TeacherID = 104;
+            teacher.TeacherName = "Mrs.Savitha";
+
+            // Act & Assert
+            Assert.AreEqual(104, teacher.TeacherID);
+            Assert.AreEqual("Mrs.Savitha", teacher.TeacherName);
+        }
+    }
+}
